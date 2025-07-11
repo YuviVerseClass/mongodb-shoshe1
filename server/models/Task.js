c333023e3f99398898c963d09c8c9abbb94d2ca3
shoshe1 <shahdbaloum2006@gmail.com>
2025-07-11 16:48:55 +0300

    Implement Task model with title and done fields using Mongoose

--- server/models/Task.js
+++ server/models/Task.js
@@ -1,5 +1,5 @@
 // Require mongoose
-
+const mongoose = require('mongoose');
 
 // Implement schema for a task
 // Each task should have:
@@ -7,4 +7,17 @@
 // - done (Boolean, defaults to false)
 
 
+const task_schema = new mongoose.Schema({
+  title:
+   { type: String, 
+    required: true
+   },
+  done: 
+  { type: Boolean, 
+    default: false
+   }
+});
+
+
 // Export the model as 'Task'
+module.exports = mongoose.model('Task', task_schema);
