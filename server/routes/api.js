92ebc4bb752dc16033bac7c823d7c05602fb1980
shoshe1 <shahdbaloum2006@gmail.com>
2025-07-11 16:39:02 +0300

    Set up Express router and require database controller in api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,8 @@
 // Require express and create a router
-
+const express = require('express');
+const router = express.Router();
 // Require your controller
+const db = require('../controllers/dbController');
 // const db = ...
 
 // Wire each of the following routes to its controller method:
