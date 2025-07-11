e81dba39101da910c9f327fe0d08ac0e05961eaf
shoshe1 <shahdbaloum2006@gmail.com>
2025-07-11 16:42:31 +0300

    Add task API routes and export router in api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,8 +7,12 @@ const db = require('../controllers/dbController');
 
 // Wire each of the following routes to its controller method:
 // GET    /tasks         => db.getTasks
+router.get('/tasks', db.getTasks);
 // POST   /tasks         => db.addTask
+router.post('/tasks', db.addTask);
 // PATCH  /tasks/:id     => db.toggleTask
+router.patch('/tasks/:id', db.toggleTask);
 // DELETE /tasks/:id     => db.deleteTask
-
+router.delete('/tasks/:id', db.deleteTask);
 // Export the router
+module.exports = router;
